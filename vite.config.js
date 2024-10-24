72f5b2b2cb15a8aea7c47e665b432116f58c8436
Carlo <CarloMehegan@gmail.com>
2024-10-24 13:03:16 -0400

    Create vite.config.js
    to hopefully fix the netlify deploy not recognizing all of the files

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        // Specifying multiple HTML files as entry points
+        main: resolve(__dirname, 'index.html'),
+        home: resolve(__dirname, 'home.html'),
+        homepage: resolve(__dirname, 'homepage.html'),
+        discussion: resolve(__dirname, 'discussion.html'),
+        draft_motion: resolve(__dirname, 'draft_motion.html'),
+        login: resolve(__dirname, 'login.html'),
+        signup: resolve(__dirname, 'signup.html'),
+        view_motions: resolve(__dirname, 'view_motions.html'),
+        // Add more pages as needed
+      },
+    },
+  },
+});
